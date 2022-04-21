c044705df5d125536f26b43c981160f398ba7eaa
Adrian Rabelo <adrianrabeloe10@gmail.com>
2022-04-21 12:08:56 -0400

    criando metodo de transferencia

--- bin/contaCorrente.dart
+++ bin/contaCorrente.dart
@@ -6,4 +6,24 @@ class ContaCorrente {
   int? conta;
   double saldo = 20.0;
   double chequeEspecial = -100.0;
+
+  bool verificaSaldo(double valor) {
+    if (saldo - valor < chequeEspecial) {
+      print('Sem saldo suficiente.');
+      return false;
+    } else {
+      print('Movimentando R\$ $valor');
+      return true;
+    }
+  }
+
+  bool transferencia(double valor, ContaCorrente contaDestino) {
+    if (!verificaSaldo(valor)) {
+      return false;
+    } else {
+      saldo -= valor;
+      contaDestino.saldo += valor;
+      return true;
+    }
+  }
 }
