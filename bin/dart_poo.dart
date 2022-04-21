bd1bff113b08f26e44149663d5eca7891f052408
Adrian Rabelo <adrianrabeloe10@gmail.com>
2022-04-21 11:32:10 -0400

    imprimindo classe

--- bin/dart_poo.dart
+++ bin/dart_poo.dart
@@ -1,12 +1,21 @@
 void main() {
-  ContaCorrente? conta;
+  ContaCorrente conta = ContaCorrente();
+  conta.titular = 'Gabriel';
+  conta.agencia = 123;
+  conta.conta = 1234;
+  conta.saldo = 150.0;
 
-  print(conta);
+  print('Titular: ${conta.titular}');
+  print('Agência: ${conta.agencia}');
+  print('Conta: ${conta.conta}');
+  print('Saldo: ${conta.saldo}');
 }
 
 class ContaCorrente {
+
   late String titular;
   late int agencia;
   late int conta;
   late double saldo;
+
 }
